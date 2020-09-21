1738964a323b2a6b8c7d1f69508547597dde145a
Ashraful Alam <wordpressasif@gmail.com>
2020-09-22 03:31:18 +0600

    service title update

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,45 +1,46 @@
-import React, {Component} from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLaptop } from '@fortawesome/free-solid-svg-icons';
-import Datajson from '../Data/content';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLaptop } from "@fortawesome/free-solid-svg-icons";
+import Datajson from "../Data/content";
 
 const servicelist = Datajson.services;
 
-
-class Service extends Component{
-
-    render(){
-      return(
-
-        <div className="page-section" id="services">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h2 className="section-heading text-uppercase">Services</h2>
-                  <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-              </div>
-              <div className="row text-center">
-          
-                {  
-                  servicelist?
-                    servicelist.map((service,i) =>
-                      <div key={i} className="col-md-4">  
-                        <span className="fa-stack fa-4x">
-                          <FontAwesomeIcon className="text-primary" icon={faLaptop} size="lg" />
-                        </span>
-                        <h4 className="service-heading">{service.title}</h4>
-                        <p className="text-muted">{service.des}</p>
-                      </div>
-                    )
-                  :<p>Please Wait..</p>
-                }
-
-              </div>
+class Service extends Component {
+  render() {
+    return (
+      <div className="page-section" id="services">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h2 className="section-heading text-uppercase">Our Services</h2>
+              <h3 className="section-subheading text-muted">
+                Lorem ipsum dolor sit amet consectetur.
+              </h3>
             </div>
+          </div>
+          <div className="row text-center">
+            {servicelist ? (
+              servicelist.map((service, i) => (
+                <div key={i} className="col-md-4">
+                  <span className="fa-stack fa-4x">
+                    <FontAwesomeIcon
+                      className="text-primary"
+                      icon={faLaptop}
+                      size="lg"
+                    />
+                  </span>
+                  <h4 className="service-heading">{service.title}</h4>
+                  <p className="text-muted">{service.des}</p>
+                </div>
+              ))
+            ) : (
+              <p>Please Wait..</p>
+            )}
+          </div>
         </div>
-      )
-    }
+      </div>
+    );
+  }
 }
 
 export default Service;
